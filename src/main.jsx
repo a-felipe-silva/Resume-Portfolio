e04f3646d72931c61fdc59d372969b7ee1ebbe2e
Agostinho Felipe <agostinhofelipeneto@outlook.com>
2025-07-17 22:08:34 -0300

    Remove custom cursor on mobile

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import CustomCursor from './components/CustomCursor';
+import { isMobile } from 'react-device-detect';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <CustomCursor />
+    {isMobile ? (
+      <CustomCursor />
+    ) : null}
   </React.StrictMode>
 );
